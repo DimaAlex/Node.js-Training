dd2d91a4043e4b4dbd1af2e74c664946bcb7f23e
DimaAlex <dimaalekseichik@gmail.com>
2016-07-26 01:43:54 +0300

    domain patr 2, error with db

--- domain/handler.js
+++ domain/handler.js
@@ -1,13 +1,16 @@
-var fs = require('fs');
+var redis = require('redis').createClient();
 
-model.exports = function handler(req, res) {
+module.exports = function handler(req, res) {
 
     if (req.url == '/') {
+        redis.get("data", process.domain.bind(function (err) {
+            throw  new Error("redis callback");
+        }));
 
-        fs.readFile('index.html', function(err, info) {
-            if (err) throw err;
-            res.end(info);
-        });
+        // fs.readFile('index.html', function(err, info) {
+        //     if (err) throw err;
+        //     res.end(info);
+        // });
 
     } else {
         res.statusCode = 404;
