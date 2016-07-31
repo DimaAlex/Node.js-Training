e172e94fcb14a87a88b629dcd8fc397389880bf0
DimaAlex <dimaalekseichik@gmail.com>
2016-08-01 00:40:46 +0300

    work with mongoose and model user

--- chat/createDB.js
+++ chat/createDB.js
@@ -1,25 +1,14 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var User = require('./models/user').User;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+var user = new User({
+  username: "Testasdasd",
+  password: "secret"
+});
 
-  var collection = db.collection('test_insert');
-  collection.remove({}, function (err, affected) {
-    if (err) throw err;
+user.save(function (err, user, affected) {
+  if (err) throw err;
 
-    collection.insert({a: 2}, function (err, docs) {
-      var cursor = collection.find({a: 2});
-      cursor.toArray(function (err, results) {
-        console.dir(results);
-
-        db.close();
-      });
-    });
-
-  });
+  User.findOne({username: "Tester"}, function (err, tester) {
+    console.log(tester);
+  })
 });
